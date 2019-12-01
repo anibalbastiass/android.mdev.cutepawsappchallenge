940fe8da451750b1e13aaec2bbcc41c4436e64ff
Anibal Bastias Soto <anibal.bastias@gmail.com>
2019-12-01 10:17:31 -0300

    Implement SearchView with filter

--- app/src/main/java/com/anibalbastias/android/cutepaws/presentation/ui/breeds/BreedsListFragment.kt
+++ app/src/main/java/com/anibalbastias/android/cutepaws/presentation/ui/breeds/BreedsListFragment.kt
@@ -1,34 +1,42 @@
 package com.anibalbastias.android.cutepaws.presentation.ui.breeds
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.ImageView
+import androidx.appcompat.widget.SearchView
 import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.MenuItemCompat
 import androidx.core.view.ViewCompat
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.SavedStateViewModelFactory
 import androidx.navigation.fragment.FragmentNavigatorExtras
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.anibalbastias.android.cutepaws.R
 import com.anibalbastias.android.cutepaws.base.module.getViewModel
 import com.anibalbastias.android.cutepaws.base.view.BaseModuleFragment
 import com.anibalbastias.android.cutepaws.databinding.FragmentBreedsListBinding
 import com.anibalbastias.android.cutepaws.presentation.appComponent
 import com.anibalbastias.android.cutepaws.presentation.getAppContext
+import com.anibalbastias.android.cutepaws.presentation.ui.breeds.filter.FilterWordListener
 import com.anibalbastias.android.cutepaws.presentation.ui.breeds.model.breeds.CutePawsItemViewData
 import com.anibalbastias.android.cutepaws.presentation.ui.breeds.model.breeds.CutePawsViewData
 import com.anibalbastias.android.cutepaws.presentation.ui.breeds.viewmodel.BreedsViewModel
 import com.anibalbastias.android.cutepaws.presentation.util.*
 import com.anibalbastias.android.cutepaws.presentation.util.adapter.base.BaseBindClickHandler
+import com.anibalbastias.android.cutepaws.presentation.util.adapter.base.SingleLayoutBindRecyclerAdapter
+
 
 /**
  * Created by anibalbastias on 2019-11-25.
  */
 
-class BreedsListFragment : BaseModuleFragment(), BaseBindClickHandler<CutePawsItemViewData> {
+class BreedsListFragment : BaseModuleFragment(),
+    BaseBindClickHandler<CutePawsItemViewData>,
+    FilterWordListener<CutePawsItemViewData> {
 
     override fun tagName(): String = this::class.java.simpleName
     override fun layoutId(): Int = R.layout.fragment_breeds_list
@@ -53,11 +61,33 @@ class BreedsListFragment : BaseModuleFragment(), BaseBindClickHandler<CutePawsIt
         binding = DataBindingUtil.bind<ViewDataBinding>(view) as FragmentBreedsListBinding
         binding.cutePawsViewModel = breedsViewModel
         binding.cutePawsItemListener = this
+        binding.cutePawsItemFilter = this
         binding.lifecycleOwner = this
 
         initToolbar()
         initViewModel()
         fetchBreeds()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        inflater.inflate(R.menu.search_menu, menu)
+        val item: MenuItem = menu.findItem(R.id.action_search)
+        val searchView: SearchView = MenuItemCompat.getActionView(item) as SearchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(query: String?): Boolean {
+                val adapter = (binding.cutePawsListRecyclerView.adapter
+                        as? SingleLayoutBindRecyclerAdapter<CutePawsItemViewData>)
+                adapter?.getFilter()?.filter(query)
+                return false
+            }
+        })
     }
 
     private fun fetchBreeds() {
@@ -150,5 +180,15 @@ class BreedsListFragment : BaseModuleFragment(), BaseBindClickHandler<CutePawsIt
 
     private fun showErrorView() {
         breedsViewModel.isError.set(true)
+    }
+
+    override fun onFilterByWord(
+        word: String,
+        selectedItem: CutePawsItemViewData,
+        filteredItems: ArrayList<CutePawsItemViewData>
+    ) {
+        if (selectedItem.breed?.contains(word, ignoreCase = true) == true) {
+            filteredItems.add(selectedItem)
+        }
     }
 }
--- app/src/main/java/com/anibalbastias/android/cutepaws/presentation/util/BindingUtils.kt
+++ app/src/main/java/com/anibalbastias/android/cutepaws/presentation/util/BindingUtils.kt
@@ -4,6 +4,8 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.anibalbastias.android.cutepaws.presentation.ui.breeds.filter.CustomFilter
+import com.anibalbastias.android.cutepaws.presentation.ui.breeds.filter.FilterWordListener
 import com.anibalbastias.android.cutepaws.presentation.util.adapter.base.BaseBindClickHandler
 import com.anibalbastias.android.cutepaws.presentation.util.adapter.base.SingleLayoutBindRecyclerAdapter
 import com.anibalbastias.android.cutepaws.presentation.util.widget.WrapContentLinearLayoutManager
@@ -22,12 +24,21 @@ fun ImageView.setImageUrlFull(imageUrl: String?) {
     }
 }
 
-@BindingAdapter(value = ["loadAdapterData", "loadAdapterLayout", "loadAdapterListener"], requireAll = false)
-fun <T> RecyclerView.loadAdapterData(list: MutableList<T>?, layout: Int?, callback: BaseBindClickHandler<T>? = null) {
+@BindingAdapter(
+    value = ["loadAdapterData", "loadAdapterLayout", "loadAdapterListener", "filter"],
+    requireAll = false
+)
+fun <T> RecyclerView.loadAdapterData(
+    list: MutableList<T>?,
+    layout: Int?,
+    callback: BaseBindClickHandler<T>? = null,
+    filter: FilterWordListener<T>? = null
+) {
     context?.run {
         layout?.let { layoutId ->
             layoutManager = GridLayoutManager(context, 2)
-            adapter = SingleLayoutBindRecyclerAdapter(layoutId, list, callback)
+            val singleAdapter = SingleLayoutBindRecyclerAdapter(layoutId, list, callback, filter)
+            adapter = singleAdapter
             runLayoutAnimation()
         }
     }
